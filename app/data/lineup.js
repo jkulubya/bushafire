1b2a718637aa4d534d99a0eca96e7a3143ed24e3
jkulubya <jameskitamirike@gmail.com>
2018-06-08 12:54:04 +0300

    Add schedule

---
+++ app/data/lineup.js
@@ -0,0 +1,648 @@
+const lineup = {
+  main: {
+    days: [
+      {
+        date: '2018-05-25',
+        start: '2018-05-25T16:00',
+        end: '2018-05-26T03:00:00',
+      },
+      {
+        date: '2018-05-26',
+        start: '2018-05-26T10:00',
+        end: '2018-05-27T04:00',
+      },
+      {
+        date: '2018-05-27',
+        start: '2018-05-27T10:00',
+        end: '2018-05-28T00:00',
+      },
+    ],
+    acts: [
+      {
+        name: "Timbila Muzimba",
+        artistes: [{ id: 0, name: "Timbila Muzimba", country: "MOZ" }],
+        start: '2018-05-25T19:20',
+        end: '2018-05-25T20:40'
+      },
+      {
+        name: "Velemseni",
+        artistes: [{ id: 0, name: "Velemseni", country: "SWZ" }],
+        start: '2018-05-25T20:40',
+        end: '2018-05-25T22:00'
+      },
+      {
+        name: "Flavia Coehlo",
+        artistes: [{ id: 0, name: "Flavia Coehlo", country: "BRA" }],
+        start: '2018-05-25T22:00',
+        end: '2018-05-25T23:20'
+      },
+      {
+        name: "Nakhane",
+        artistes: [{ id: 0, name: "Nakhane", country: "RSA" }],
+        start: '2018-05-25T23:20',
+        end: '2018-05-26T00:40'
+      },
+      {
+        name: "Dub Inc",
+        artistes: [{ id: 0, name: "Dub Inc", country: "FRA" }],
+        start: '2018-05-26T00:40',
+        end: '2018-05-26T02:00'
+      },
+      {
+        name: "Khuli Chana",
+        artistes: [{ id: 0, name: "Khuli Chana", country: "RSA" }],
+        start: '2018-05-26T02:00',
+        end: '2018-05-26T03:00'
+      },
+      {
+        name: "Azah",
+        artistes: [{ id: 0, name: "Azah", country: "RSA" }],
+        start: '2018-05-26T13:50',
+        end: '2018-05-26T15:10'
+      },
+      {
+        name: "Alice Phoebe Lou",
+        artistes: [{ id: 0, name: "Alice Phoebe Lou", country: "RSA" }],
+        start: '2018-05-26T15:10',
+        end: '2018-05-26T16:30'
+      },
+      {
+        name: "Samthing Soweto",
+        artistes: [{ id: 0, name: "Samthing Soweto", country: "RSA" }],
+        start: '2018-05-26T16:30',
+        end: '2018-05-26T17:50'
+      },
+      {
+        name: "Sands",
+        artistes: [{ id: 0, name: "Sands", country: "SWZ" }],
+        start: '2018-05-26T17:50',
+        end: '2018-05-26T19:10'
+      },
+      {
+        name: "Elida Almeida",
+        artistes: [{ id: 0, name: "Elida Almeida", country: "REU" }],
+        start: '2018-05-26T19:10',
+        end: '2018-05-26T20:30'
+      },
+      {
+        name: "Dear Ribane & OKZharp",
+        artistes: [{ id: 0, name: "Dear Ribane & OKZharp", country: "RSA" }],
+        start: '2018-05-26T20:30',
+        end: '2018-05-26T21:50'
+      },
+      {
+        name: "Kapa Dech",
+        artistes: [{ id: 0, name: "Kapa Dech", country: "MOZ" }],
+        start: '2018-05-26T21:50',
+        end: '2018-05-26T23:10'
+      },
+      {
+        name: "Kwesta",
+        artistes: [{ id: 0, name: "Kwesta", country: "RSA" }],
+        start: '2018-05-26T23:10',
+        end: '2018-05-27T00:30'
+      },
+      {
+        name: "Sho Madjozi",
+        artistes: [{ id: 0, name: "Sho Madjozi", country: "RSA" }],
+        start: '2018-05-27T00:30',
+        end: '2018-05-27T01:50'
+      },
+      {
+        name: "Crimson House",
+        artistes: [{ id: 0, name: "Crimson House", country: "RSA" }],
+        start: '2018-05-27T01:50',
+        end: '2018-05-27T02:55'
+      },
+      {
+        name: "DJ Lag",
+        artistes: [{ id: 0, name: "DJ Lag", country: "RSA" }],
+        start: '2018-05-27T02:55',
+        end: '2018-05-27T04:00'
+      },
+      {
+        name: "Tlale Makhene",
+        artistes: [{ id: 0, name: "Tlale Makhene", country: "SWZ" }],
+        start: '2018-05-27T12:20',
+        end: '2018-05-27T13:40'
+      },
+      {
+        name: "Seykou Kouyate",
+        artistes: [{ id: 0, name: "Seykou Kouyate", country: "GIN" }],
+        start: '2018-05-27T13:40',
+        end: '2018-05-27T15:00'
+      },
+      {
+        name: "\"Africa Tribute to Eswatini\"",
+        artistes: [{ id: 0, name: "Salif Keita", country: "MLI" }, { id: 0, name: "Ladysmith Black Mambazo", country: "RSA" }, { id: 0, name: "Yemi Alade", country: "NGA" }],
+        start: '2018-05-27T15:00',
+        end: '2018-05-27T16:50'
+      },
+      {
+        name: "Sipho Hotstix Mabuse",
+        artistes: [{ id: 0, name: "Sipho Hotstix Mabuse", country: "RSA" }],
+        start: '2018-05-27T16:50',
+        end: '2018-05-27T18:10'
+      },
+      {
+        name: "Rouge",
+        artistes: [{ id: 0, name: "Rouge", country: "RSA" }],
+        start: '2018-05-27T18:10',
+        end: '2018-05-28T00:00'
+      },
+    ]
+  },
+  amphitheatre: {
+    days: [
+      {
+        date: '2018-05-25',
+        start: '2018-05-25T16:00',
+        end: '2018-05-26T03:00:00',
+      },
+      {
+        date: '2018-05-26',
+        start: '2018-05-26T10:00',
+        end: '2018-05-27T04:00',
+      },
+      {
+        date: '2018-05-27',
+        start: '2018-05-27T10:00',
+        end: '2018-05-28T00:00',
+      },
+    ],
+    acts: [
+      {
+        name: "Mario Batkovic",
+        artistes: [{ id: 0, name: "Mario Batkovic", country: "CHE" }],
+        start: '2018-05-25T18:15',
+        end: '2018-05-25T20:25'
+      },
+      {
+        name: "Oki Dub Ainu Band",
+        artistes: [{ id: 0, name: "Oki Dub Ainu Band", country: "JPN" }],
+        start: '2018-05-25T20:25',
+        end: '2018-05-25T21:45'
+      },
+      {
+        name: "Nathalie Nathiembe",
+        artistes: [{ id: 0, name: "Nathalie Nathiembe", country: "REU" }],
+        start: '2018-05-25T21:45',
+        end: '2018-05-25T23:05'
+      },
+      {
+        name: "Georgetown",
+        artistes: [{ id: 0, name: "Georgetown", country: "RSA" }],
+        start: '2018-05-25T23:05',
+        end: '2018-05-26T00:25'
+      },
+      {
+        name: "Dusty & Stones",
+        artistes: [{ id: 0, name: "Dusty & Stones", country: "SWZ" }],
+        start: '2018-05-26T00:25',
+        end: '2018-05-26T01:45'
+      },
+      {
+        name: "Liyana Film Screening",
+        artistes: [],
+        start: '2018-05-26T10:45',
+        end: '2018-05-26T13:00'
+      },
+      {
+        name: "Salsa Dance Session",
+        artistes: [],
+        start: '2018-05-26T13:00',
+        end: '2018-05-26T14:55'
+      },
+      {
+        name: "Guy Battery & Kanada Narahari",
+        artistes: [{ id: 0, name: "Guy Battery", country: "RSA" }, { id: 0, name: "Kanada Narahari", country: "IND" }],
+        start: '2018-05-26T14:55',
+        end: '2018-05-26T16:15'
+      },
+      {
+        name: "Derek Gripper",
+        artistes: [{ id: 0, name: "Derek Gripper", country: "RSA" }],
+        start: '2018-05-26T16:15',
+        end: '2018-05-26T17:35'
+      },
+      {
+        name: "5k-HD",
+        artistes: [{ id: 0, name: "5k-HD", country: "RSA" }],
+        start: '2018-05-26T17:35',
+        end: '2018-05-26T18:55'
+      },
+      {
+        name: "Nonku Phiri",
+        artistes: [{ id: 0, name: "Nonku Phiri", country: "RSA" }],
+        start: '2018-05-26T18:55',
+        end: '2018-05-26T20:15'
+      },
+      {
+        name: "The Grammers",
+        artistes: [{ id: 0, name: "The Grammers", country: "FIN" }],
+        start: '2018-05-26T20:15',
+        end: '2018-05-26T21:35'
+      },
+      {
+        name: "Ibaaku",
+        artistes: [{ id: 0, name: "Ibaaku", country: "SEN" }],
+        start: '2018-05-26T21:35',
+        end: '2018-05-26T22:55'
+      },
+      {
+        name: "Native Young",
+        artistes: [{ id: 0, name: "Native Young", country: "RSA" }],
+        start: '2018-05-26T22:55',
+        end: '2018-05-27T00:15'
+      },
+      {
+        name: "M. Triggerson",
+        artistes: [{ id: 0, name: "M. Triggerson", country: "SWZ" }],
+        start: '2018-05-27T00:15',
+        end: '2018-05-27T01:35'
+      },
+      {
+        name: "Radio 123",
+        artistes: [{ id: 0, name: "Radio 123", country: "RSA" }],
+        start: '2018-05-27T01:35',
+        end: '2018-05-27T02:55'
+      },
+      {
+        name: "Cybos",
+        artistes: [{ id: 0, name: "Cybos", country: "SWZ" }],
+        start: '2018-05-27T02:55',
+        end: '2018-05-27T04:00'
+      },
+      {
+        name: "Staceyann Chin",
+        artistes: [{ id: 0, name: "Staceyann Chin", country: "JAM" }],
+        start: '2018-05-27T11:00',
+        end: '2018-05-27T14:00'
+      },
+      {
+        name: "Salsa Dance Session",
+        artistes: [],
+        start: '2018-05-27T14:00',
+        end: '2018-05-27T19:30'
+      },
+      {
+        name: "DJ Doo Wap & K$ - Official After Party",
+        artistes: [{ id: 0, name: "DJ Doo Wap & K$", country: "RSA" }],
+        start: '2018-05-27T19:30',
+        end: '2018-05-28T00:00'
+      },
+    ]
+  },
+  barn: {
+    days: [
+      {
+        date: '2018-05-25',
+        start: '2018-05-25T16:00',
+        end: '2018-05-26T03:00:00',
+      },
+      {
+        date: '2018-05-26',
+        start: '2018-05-26T10:00',
+        end: '2018-05-27T04:00',
+      },
+      {
+        date: '2018-05-27',
+        start: '2018-05-27T10:00',
+        end: '2018-05-28T00:00',
+      },
+    ],
+    acts: [
+      {
+        name: "DJ Bob's Jazz Club",
+        artistes: [{ id: 0, name: "DJ Bob's Jazz Club", country: "RSA" }],
+        start: '2018-05-25T16:00',
+        end: '2018-05-25T20:00'
+      },
+      {
+        name: "Buddy Masango",
+        artistes: [{ id: 0, name: "Buddy Masango", country: "SWZ" }],
+        start: '2018-05-25T20:00',
+        end: '2018-05-25T21:30'
+      },
+      {
+        name: "Mr Freddy",
+        artistes: [{ id: 0, name: "Mr Freddy", country: "SWZ" }],
+        start: '2018-05-25T21:30',
+        end: '2018-05-25T23:00'
+      },
+      {
+        name: "Mario Batkovic",
+        artistes: [{ id: 0, name: "Mario Batkovic", country: "CHE" }],
+        start: '2018-05-25T23:00',
+        end: '2018-05-26T04:00'
+      },
+      {
+        name: "DJ Bob's Jazz Club",
+        artistes: [{ id: 0, name: "DJ Bob's Jazz Club", country: "RSA" }],
+        start: '2018-05-26T10:00',
+        end: '2018-05-26T12:45'
+      },
+      {
+        name: "Khoisanboy_Man",
+        artistes: [{ id: 0, name: "Khoisanboy_Man", country: "RSA" }],
+        start: '2018-05-26T12:45',
+        end: '2018-05-26T16:15'
+      },
+      {
+        name: "Open Mic",
+        artistes: [],
+        start: '2018-05-26T16:15',
+        end: '2018-05-26T17:35'
+      },
+      {
+        name: "Manu Sija",
+        artistes: [{ id: 0, name: "Manu Sija", country: "ARG" }],
+        start: '2018-05-26T17:35',
+        end: '2018-05-26T19:30'
+      },
+      {
+        name: "Albert Frost",
+        artistes: [{ id: 0, name: "Albert Frost", country: "RSA" }],
+        start: '2018-05-26T19:30',
+        end: '2018-05-26T20:45'
+      },
+      {
+        name: "Champions League",
+        artistes: [],
+        start: '2018-05-26T20:45',
+        end: '2018-05-26T23:00'
+      },
+      {
+        name: "Bam Bam Brown",
+        artistes: [{ id: 0, name: "Bam Bam Brown", country: "RSA" }],
+        start: '2018-05-26T23:00',
+        end: '2018-05-27T00:20'
+      },
+      {
+        name: "The Lions",
+        artistes: [{ id: 0, name: "The Lions", country: "SWZ" }],
+        start: '2018-05-27T00:20',
+        end: '2018-05-27T04:00'
+      },
+      {
+        name: "DJ Bob's Jazz Club",
+        artistes: [{ id: 0, name: "DJ Bob's Jazz Club", country: "RSA" }],
+        start: '2018-05-27T10:00',
+        end: '2018-05-27T11:00'
+      },
+      {
+        name: "Bholoja",
+        artistes: [{ id: 0, name: "Bholoja", country: "SWZ" }],
+        start: '2018-05-27T11:00',
+        end: '2018-05-27T12:15'
+      },
+      {
+        name: "Open Mic",
+        artistes: [],
+        start: '2018-05-27T12:15',
+        end: '2018-05-27T13:25'
+      },
+      {
+        name: "Sibusile Xaba",
+        artistes: [{ id: 0, name: "Sibusile Xaba", country: "RSA" }],
+        start: '2018-05-27T13:25',
+        end: '2018-05-28T00:00'
+      },      
+    ]
+  },
+  firefly: {
+    days: [
+      {
+        date: '2018-05-25',
+        start: '2018-05-25T16:00',
+        end: '2018-05-26T03:00:00',
+      },
+      {
+        date: '2018-05-26',
+        start: '2018-05-26T10:00',
+        end: '2018-05-27T04:00',
+      },
+      {
+        date: '2018-05-27',
+        start: '2018-05-27T10:00',
+        end: '2018-05-28T00:00',
+      },
+    ],
+    acts: [
+      {
+        name: "DJ Ohpis",
+        artistes: [{ id: 0, name: "DJ Ohpis", country: "SWZ" }],
+        start: '2018-05-25T18:30',
+        end: '2018-05-25T19:30'
+      },
+      {
+        name: "Jews for Techno",
+        artistes: [{ id: 0, name: "Jews for Techno", country: "RSA" }],
+        start: '2018-05-25T19:30',
+        end: '2018-05-25T20:30'
+      },
+      {
+        name: "Paluku the Scribe",
+        artistes: [{ id: 0, name: "Paluku the Scribe", country: "SWZ" }],
+        start: '2018-05-25T20:30',
+        end: '2018-05-25T20:45'
+      },
+      {
+        name: "DJ Mavava",
+        artistes: [{ id: 0, name: "DJ Mavava", country: "SWZ/RSA" }],
+        start: '2018-05-25T20:45',
+        end: '2018-05-25T21:45'
+      },
+      {
+        name: "DJ Crooks",
+        artistes: [{ id: 0, name: "DJ Crooks", country: "SWZ" }],
+        start: '2018-05-25T21:45',
+        end: '2018-05-25T22:45'
+      },
+      {
+        name: "Karyendasoul",
+        artistes: [{ id: 0, name: "Karyendasoul", country: "RSA" }],
+        start: '2018-05-25T22:45',
+        end: '2018-05-25T23:45'
+      },
+      {
+        name: "Costa Titch",
+        artistes: [{ id: 0, name: "Costa Titch", country: "RSA" }],
+        start: '2018-05-25T23:45',
+        end: '2018-05-26T00:00'
+      },
+      {
+        name: "DJ Mkay",
+        artistes: [{ id: 0, name: "DJ Mkay", country: "SWZ/KEN" }],
+        start: '2018-05-26T00:00',
+        end: '2018-05-26T01:00'
+      },
+      {
+        name: "DJ Mbally",
+        artistes: [{ id: 0, name: "DJ Mbally", country: "SWZ/RSA" }],
+        start: '2018-05-26T01:00',
+        end: '2018-05-26T02:00'
+      },
+      {
+        name: "DJ Lu",
+        artistes: [{ id: 0, name: "DJ Lu", country: "SWZ" }],
+        start: '2018-05-26T02:00',
+        end: '2018-05-26T03:00'
+      },
+      {
+        name: "Paul Waxon",
+        artistes: [{ id: 0, name: "Paul Waxon", country: "RSA" }],
+        start: '2018-05-26T13:00',
+        end: '2018-05-26T14:00'
+      },
+      {
+        name: "DJ Mbally",
+        artistes: [{ id: 0, name: "DJ Mbally", country: "SWZ/RSA" }],
+        start: '2018-05-26T14:00',
+        end: '2018-05-26T15:00'
+      },
+      {
+        name: "Jews for Techno",
+        artistes: [{ id: 0, name: "Jews for Techno", country: "RSA" }],
+        start: '2018-05-26T15:00',
+        end: '2018-05-26T16:00'
+      },
+      {
+        name: "Davina Satori",
+        artistes: [{ id: 0, name: "Davina Satori", country: "RSA" }],
+        start: '2018-05-26T16:00',
+        end: '2018-05-26T17:00'
+      },
+      {
+        name: "DJ Czar",
+        artistes: [{ id: 0, name: "DJ Czar", country: "SWZ" }],
+        start: '2018-05-26T17:00',
+        end: '2018-05-26T18:00'
+      },
+      {
+        name: "DJ Mkay",
+        artistes: [{ id: 0, name: "DJ Mkay", country: "SWZ/KEN" }],
+        start: '2018-05-26T18:00',
+        end: '2018-05-26T19:15'
+      },
+      {
+        name: "Ezamaswazi",
+        artistes: [{ id: 0, name: "Ezamaswazi", country: "SWZ" }],
+        start: '2018-05-26T19:15',
+        end: '2018-05-26T20:30'
+      },
+      {
+        name: "Miss Trophy",
+        artistes: [{ id: 0, name: "Miss Trophy", country: "SWZ" }],
+        start: '2018-05-26T20:30',
+        end: '2018-05-26T20:45'
+      },
+      {
+        name: "Mzwaa",
+        artistes: [{ id: 0, name: "Mzwaa", country: "SWZ" }],
+        start: '2018-05-26T20:45',
+        end: '2018-05-26T21:00'
+      },
+      {
+        name: "Speech Umlangeni",
+        artistes: [{ id: 0, name: "Speech Umlangeni", country: "SWZ" }],
+        start: '2018-05-26T21:00',
+        end: '2018-05-26T22:00'
+      },
+      {
+        name: "Cornelius",
+        artistes: [{ id: 0, name: "Cornelius", country: "RSA" }],
+        start: '2018-05-26T22:00',
+        end: '2018-05-26T23:00'
+      },
+      {
+        name: "DJ Touch of Soul",
+        artistes: [{ id: 0, name: "DJ Touch of Soul", country: "SWZ" }],
+        start: '2018-05-26T23:00',
+        end: '2018-05-27T00:00'
+      },
+      {
+        name: "DJ Master P",
+        artistes: [{ id: 0, name: "DJ Master P", country: "SWZ" }],
+        start: '2018-05-27T00:00',
+        end: '2018-05-27T01:00'
+      },
+      {
+        name: "Mobi Dixon",
+        artistes: [{ id: 0, name: "Mobi Dixon", country: "RSA" }],
+        start: '2018-05-27T01:00',
+        end: '2018-05-27T02:00'
+      },
+      {
+        name: "Zulu D",
+        artistes: [{ id: 0, name: "Zulu D", country: "SWZ" }],
+        start: '2018-05-27T02:00',
+        end: '2018-05-27T04:00'
+      },
+      {
+        name: "Paul Waxon",
+        artistes: [{ id: 0, name: "Paul Waxon", country: "RSA" }],
+        start: '2018-05-27T13:00',
+        end: '2018-05-27T14:00'
+      },
+      {
+        name: "DJ Lu",
+        artistes: [{ id: 0, name: "DJ Lu", country: "SWZ" }],
+        start: '2018-05-27T14:00',
+        end: '2018-05-27T15:00'
+      },
+      {
+        name: "DJ Mavava",
+        artistes: [{ id: 0, name: "DJ Mavava", country: "SWZ" }],
+        start: '2018-05-27T15:00',
+        end: '2018-05-27T16:00'
+      },
+      {
+        name: "KaryendaSoul",
+        artistes: [{ id: 0, name: "KaryendaSoul", country: "RSA" }],
+        start: '2018-05-27T16:00',
+        end: '2018-05-27T17:00'
+      },
+      {
+        name: "DJ Ohpis",
+        artistes: [{ id: 0, name: "DJ Ohpis", country: "SWZ" }],
+        start: '2018-05-27T17:00',
+        end: '2018-05-27T18:00'
+      },
+      {
+        name: "DJ Czar",
+        artistes: [{ id: 0, name: "DJ Czar", country: "SWZ" }],
+        start: '2018-05-27T18:00',
+        end: '2018-05-27T19:00'
+      },
+      {
+        name: "DJ Crooks",
+        artistes: [{ id: 0, name: "DJ Crooks", country: "SWZ" }],
+        start: '2018-05-27T19:00',
+        end: '2018-05-27T20:00'
+      },
+      {
+        name: "Speech Umlangeni",
+        artistes: [{ id: 0, name: "Speech Umlangeni", country: "SWZ" }],
+        start: '2018-05-27T20:00',
+        end: '2018-05-27T21:00'
+      },
+      {
+        name: "DJ Swizz",
+        artistes: [{ id: 0, name: "DJ Swizz", country: "SWZ" }],
+        start: '2018-05-27T21:00',
+        end: '2018-05-27T22:00'
+      },
+      {
+        name: "Zulu D",
+        artistes: [{ id: 0, name: "Zulu D", country: "SWZ" }],
+        start: '2018-05-27T22:00',
+        end: '2018-05-28T00:00'
+      },
+    ]
+  },
+}
+
+// All times in Africa/Mbabane
+
+export default lineup;
